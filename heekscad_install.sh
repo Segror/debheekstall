efe7d1ccf29782de4a652c6afd13b9ded71cc7c8
Paul Kremser <paul.kremser@cern.ch>
2014-07-21 23:43:02 +0200

    First Version.
    - maybe some dependencies are still missing
    - some more sanity and maybe options (like working dir) todo

---
+++ heekscad_install.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+DEPENDS="subversion fakeroot  dpkg-dev libboost-python-dev liboce-*"
+DEPENDS="$DEPENDS oce-draw libgtkglext1-dev libboost-dev"
+WORK_DIR="${HOME}/heeks"
+MAKEFLAGS="-j2"
+
+echo " _   _           _         ____    _    ____  "
+echo "| | | | ___  ___| | _____ / ___|  / \  |  _ \ "
+echo "| |_| |/ _ \/ _ \ |/ / __| |     / _ \ | | | |"
+echo "|  _  |  __/  __/   <\__ \ |___ / ___ \| |_| |"
+echo "|_| |_|\___|\___|_|\_\___/\____/_/   \_\____(_) "
+echo "          Install or Update Script for "
+echo "              Debian based Distros"
+echo ""
+
+if [ "$1" != "--update-or-install" ]; then
+    echo "usage: $0 --update-or-install"
+    echo ""
+    echo "This script downloads and installs opencamlib from github,"
+    echo "libarea, heekscad and heekscnc from googlecode on debian"
+    echo "based linux distributions. All is packeged as .deb and "
+    echo "installed via dpgk."
+    echo ""
+    echo "Source is downloaded to:"
+    echo "      $WORK_DIR"
+    exit 1
+fi
+
+echo " --> Installing dependencies ..."
+sudo apt-get install $DEPENDS || exit -1
+
+echo " --> Getting sources ..."
+mkdir -p $WORK_DIR 2>/dev/zero
+cd $WORK_DIR
+rm *.deb 2>/dev/zero
+
+if [ -d "${WORK_DIR}/heekscad" ]; then
+    cd "${WORK_DIR}/heekscad" && svn update
+else
+    cd $WORK_DIR
+    svn checkout http://heekscad.googlecode.com/svn/trunk/ heekscad
+fi
+if [ -d "${WORK_DIR}/heekscnc" ]; then
+    cd "${WORK_DIR}/heekscnc" && svn update
+else
+    cd $WORK_DIR
+    svn checkout http://heekscnc.googlecode.com/svn/trunk/ heekscnc
+fi
+if [ -d "${WORK_DIR}/libarea" ]; then
+    cd "${WORK_DIR}/libarea" && svn update
+else
+    cd $WORK_DIR
+    svn checkout http://libarea.googlecode.com/svn/trunk/ libarea
+fi
+
+echo " --> Building libarea ..."
+cd "${WORK_DIR}/libarea"
+dpkg-buildpackage -b
+cd .. && sudo dpkg -i libarea*.deb python-area*.deb
+
+echo " --> Building heekscad ..."
+cd "${WORK_DIR}/heekscad"
+dpkg-buildpackage -b
+cd .. && sudo dpkg -i heekscad*.deb libheekstinyxml*.deb
+
+echo " --> Building heekscnc ..."
+cd "${WORK_DIR}"
+git clone https://github.com/aewallin/opencamlib.git
+cd opencamlib
+bzr branch lp:~neomilium/opencamlib/packaging debian
+dpkg-buildpackage -b -us -uc
+cd .. && sudo dpkg -i python-ocl*.deb
+
+cd "${WORK_DIR}/heekscnc"
+dpkg-buildpackage -b
+cd .. && sudo dpkg -i heekscnc*.deb
